79a95bdf0e12eeff1f5927a2f46fc23ebd99c6c2
xander98in <leyderin@unicauca.edu.co>
2024-05-03 18:05:49 -0500

    Backend en nest

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,75 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectModel( User.name )
+    private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    console.log(createUserDto);
+    
+    try {
+
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+ 
+      console.log(newUser);
+      
+      await newUser.save();
+      const { password:_, ...user } = newUser.toJSON();
+
+      return user;
+    }
+    catch( error ) {
+      if( error.code === 11000 ) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+      throw new InternalServerErrorException('Something terrible happen!!!!');
+    }
+    
+  }
+
+  async login(loginDto: LoginDto): Promise<> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({email});
+    if(!user) {
+      throw new UnauthorizedException('Not valid credentials - email');
+    }
+
+    if(!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials - password')
+    }
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
